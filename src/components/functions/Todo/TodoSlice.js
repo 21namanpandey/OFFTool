30b8a2c5eab617c40d7ec90f2ba79566b34c981a
Naman Pandey <2492004namanpandey@gmail.com>
2023-12-14 22:46:31 +0530

    Added Edit Button

--- src/components/functions/Todo/TodoSlice.js
+++ src/components/functions/Todo/TodoSlice.js
@@ -1,9 +1,9 @@
-import { createSlice , nanoid } from '@reduxjs/toolkit';
+import { createSlice, nanoid } from '@reduxjs/toolkit';
 
 
 
 const initialState = {
-    todos: [{id: 1, text: "Achieve your goals !!"}]
+    todos: [{ id: 1, text: "Achieve your goals !!", updateMode: false }]
 }
 
 export const todoSlice = createSlice({
@@ -12,17 +12,37 @@ export const todoSlice = createSlice({
     reducers: {
         addTodo: (state, action) => {
             const todo = {
-                id: nanoid(), 
+                id: nanoid(),
                 text: action.payload
             }
             state.todos.push(todo)
         },
+
         removeTodo: (state, action) => {
             state.todos = state.todos.filter((todo) => todo.id !== action.payload)
+        },
+
+        updateTodo: (state, action) => {
+            state.todos.map((todo) => {
+                if (todo.id === action.payload[0]) {
+                    todo.text = action.payload[1];
+                }
+            });
+        },
+
+
+        toggleUpdateMode: (state, action) => {
+            state.todos.map((todo) => {
+                if (todo.id === action.payload) {
+                    todo.updateMode = !todo.updateMode;
+                }
+            });
+
+            state.updateTodoId = action.payload;
         },
     }
 })
 
-export const {addTodo, removeTodo} = todoSlice.actions
+export const { addTodo, removeTodo, updateTodo, toggleUpdateMode } = todoSlice.actions
 
 export default todoSlice.reducer
